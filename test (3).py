6de7c581fd49f75e4ec52e45f8386fb444924bb6
ShellEncrypt <75524570+ShellEncrypt@users.noreply.github.com>
2021-04-08 09:51:36 +0300

    Delete test (3).py

--- test (3).py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('1797122575:AAF6U8L8Wpq61p0VnjpSiZ6vSa7rNjVdhd8')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'hello')
-
-bot.polling()
